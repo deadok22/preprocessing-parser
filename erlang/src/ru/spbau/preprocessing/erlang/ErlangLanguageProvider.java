2f306afa26841708a5aff9b8520018161abfd69d
Sergey Savenko <deadok22@gmail.com>
2014-03-27 17:37:16 +0400

    implement a language provider for erlang

---
+++ erlang/src/ru/spbau/preprocessing/erlang/ErlangLanguageProvider.java
@@ -0,0 +1,20 @@
+package ru.spbau.preprocessing.erlang;
+
+import ru.spbau.preprocessing.api.LanguageLexer;
+import ru.spbau.preprocessing.api.LanguageProvider;
+import ru.spbau.preprocessing.api.preprocessor.PreprocessorLanguageParser;
+import ru.spbau.preprocessing.erlang.preprocessor.ErlangPreprocessorLanguageParser;
+
+import java.io.Reader;
+
+public class ErlangLanguageProvider implements LanguageProvider<ErlangToken> {
+  @Override
+  public LanguageLexer<ErlangToken> createLanguageLexer() {
+    return new ErlangLexer((Reader) null);
+  }
+
+  @Override
+  public PreprocessorLanguageParser createPreprocessorLanguageParser() {
+    return new ErlangPreprocessorLanguageParser();
+  }
+}
