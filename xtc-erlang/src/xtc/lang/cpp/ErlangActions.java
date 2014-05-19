4c35a8f11532659d2fa3eb888bca723e960fbf00
Sergey Savenko <deadok22@gmail.com>
2014-05-19 20:48:25 +0400

    implement xtc erlang Actions.getValue

--- xtc-erlang/src/xtc/lang/cpp/ErlangActions.java
+++ xtc-erlang/src/xtc/lang/cpp/ErlangActions.java
@@ -1,6 +1,8 @@
 package xtc.lang.cpp;
 
 import ru.spbau.preprocessing.xtc.erlang.ErlangActionsBase;
+import xtc.tree.GNode;
+import xtc.util.Pair;
 import xtc.util.Runtime;
 
 public class ErlangActions extends ErlangActionsBase {
@@ -18,5 +20,10 @@ public class ErlangActions extends ErlangActionsBase {
   @Override
   public Context getInitialContext() {
     return new CParsingContext(myRuntime);
+  }
+
+  @Override
+  public Object getValue(int id, String name, Pair<Object> values) {
+    return values == Pair.empty() ? null : GNode.createFromPair(name, values.head(), values.tail());
   }
 }
