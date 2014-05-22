c3ca308907e8a7a3d8efd6d4b03788c7afe3a09f
Sergey Savenko <deadok22@gmail.com>
2014-05-22 16:58:28 +0400

    more parser constructs (unchecked)

--- TypeChef-erlang/src/ru/spbau/preprocessing/typechef/erlang/AST.scala
+++ TypeChef-erlang/src/ru/spbau/preprocessing/typechef/erlang/AST.scala
@@ -2,27 +2,46 @@ package ru.spbau.preprocessing.typechef.erlang
 
 import de.fosd.typechef.parser.WithPosition
 import de.fosd.typechef.conditional.Opt
+import ru.spbau.preprocessing.erlang.ErlangToken
 
 trait AST extends Product with Serializable with Cloneable with WithPosition {
   override def clone(): AST.this.type = super.clone().asInstanceOf[AST.this.type]
 }
 
-
+//file
 trait Form extends AST
 
+case class File(forms: List[Opt[Form]]) extends AST
+
+
+//expressions
 trait Expr extends AST
 
+case class Atom(atom: String) extends Expr
 
-case class Atomic(atom: String) extends Expr
+case class Variable(variable: String) extends Expr
+
+case class PrefixExpr(expr: Expr, op: ErlangToken) extends Expr
+
+case class BinaryExpr(left: Expr, right: Expr, op: ErlangToken) extends Expr
+
+case class ParenthesizedExpr(expr: Expr) extends Expr
+
+case class BeginEndExpr(exprs: Exprs) extends Expr
+
+case class IntegerExpr(integer: String) extends Expr
+
+case class CharExpr(char: String) extends Expr
+
+case class FloatExpr(float: String) extends Expr
+
+case class StringsExpr(strings: List[Opt[String]]) extends Expr
 
 case class Exprs(exprs: List[Opt[Expr]]) extends Expr
 
-
+//function
 case class FunctionClause(atom: String, params: ArgumentsList, body: Exprs) extends AST
 
 case class ArgumentsList(arguments: Option[Exprs]) extends AST
 
 case class Function(clauses: List[Opt[FunctionClause]]) extends Form
-
-
-case class File(forms: List[Opt[Form]]) extends AST
--- TypeChef-erlang/src/ru/spbau/preprocessing/typechef/erlang/ErlangParser.scala
+++ TypeChef-erlang/src/ru/spbau/preprocessing/typechef/erlang/ErlangParser.scala
@@ -31,11 +31,13 @@ class ErlangParser extends MultiFeatureParser {
     new Function(_)
   }
 
-  def functionClause: MultiParser[FunctionClause] = atom ~ argumentsList ~ clauseBody ^^ {
+  def functionClause: MultiParser[FunctionClause] = atomToken ~ argumentsList ~ clauseBody ^^ {
     case clauseName ~ params ~ body => new FunctionClause(clauseName, params, body)
   }
 
-  def argumentsList = (lpar ~> exprs?) <~ rpar ^^ { new ArgumentsList(_) }
+  def argumentsList = (lpar ~> exprs ?) <~ rpar ^^ {
+    new ArgumentsList(_)
+  }
 
   def clauseBody = arrow ~> exprs
 
@@ -43,33 +45,123 @@ class ErlangParser extends MultiFeatureParser {
     new Exprs(_)
   }
 
-  def expr = atomic
+  def expr: MultiParser[Expr] = uop(CATCH, expr) | expr_100
 
-  def atomic = atom ^^ {
-    new Atomic(_)
+  def expr_100: MultiParser[Expr] = assignment | send | expr_150
+
+  def assignment = bop(expr_150, OP_EQ, expr_100)
+
+  def send = bop(expr_150, OP_EXL, expr_100)
+
+  def expr_150: MultiParser[Expr] = orelse | expr_160
+
+  def orelse = bop(expr_160, ORELSE, expr_150)
+
+  def expr_160: MultiParser[Expr] = andalso | expr_160
+
+  def andalso = bop(expr_200, ANDALSO, expr_160)
+
+  def expr_200: MultiParser[Expr] = bop(expr_300, comp_op, expr_300) | expr_300
+
+  def comp_op = et(OP_EQ_EQ) | et(OP_DIV_EQ) | et(OP_EQ_LT) | et(OP_LT) | et(OP_GT_EQ) | et(OP_GT) | et(OP_EQ_COL_EQ) | et(OP_EQ_DIV_EQ)
+
+  def expr_300: MultiParser[Expr] = bop(expr_400, list_op, expr_300) | expr_400
+
+  def list_op = et(OP_PLUS_PLUS) | et(OP_MINUS_MINUS)
+
+  def expr_400: MultiParser[Expr] = bop(expr_400, add_op, expr_500) | expr_500
+
+  def add_op = et(OP_PLUS) | et(OP_MINUS) | et(BOR) | et(BXOR) | et(BSL) | et(BSR) | et(OR) | et(XOR)
+
+  def expr_500: MultiParser[Expr] = bop(expr_500, mult_op, expr_600) | expr_600
+
+  def mult_op = et(OP_AR_DIV) | et(OP_AR_MUL) | et(DIV) | et(REM) | et(BAND) | et(AND)
+
+  def expr_600: MultiParser[Expr] = uop(prefix_op, expr_700) | expr_700
+
+  def prefix_op = et(OP_PLUS) | et(OP_MINUS) | et(BNOT) | et(NOT)
+
+  def expr_700: MultiParser[Expr] = //TODO uncomment and add parsers: function_call | record_expr | 
+    expr_800
+
+  def expr_800 = bop(expr_max, COLON, expr_max) | expr_max
+
+  def expr_max = variable | atomic | parenthesized | begin_end
+
+  def parenthesized = ((lpar ~> expr) <~ rpar) ^^ {
+    new ParenthesizedExpr(_)
   }
+
+  def begin_end = (et(BEGIN) ~> exprs) <~ et(END) ^^ {
+    new BeginEndExpr(_)
+  }
+
+  def variable = variableToken ^^ {
+    new Variable(_)
+  }
+
+  def atomic = char | integer | float | atom | strings
+
+  def atom = atomToken ^^ {
+    new Atom(_)
+  }
+
+  def char = ett("char", CHAR) ^^ {
+    new CharExpr(_)
+  }
+
+  def integer = ett("int", INTEGER) ^^ {
+    new IntegerExpr(_)
+  }
+
+  def float = ett("float", FLOAT) ^^ {
+    new FloatExpr(_)
+  }
+
+  def strings = repOpt(ett("string", STRING)) ^^ {
+    new StringsExpr(_)
+  }
+
+  def bop[Left <: Expr, Right <: Expr](l: MultiParser[Left], op: ErlangToken, r: MultiParser[Right]): MultiParser[BinaryExpr] =
+    bop(l, et(op.toString.toLowerCase, op), r)
+
+  def bop[Left <: Expr, Right <: Expr](l: MultiParser[Left], op: MultiParser[Elem], r: MultiParser[Right]): MultiParser[BinaryExpr] =
+    l ~ op ~ r ^^ {
+      case a ~ o ~ b => new BinaryExpr(a, b, o.getType)
+    }
+
+  def uop[Arg <: Expr](op: ErlangToken, a: MultiParser[Arg]): MultiParser[PrefixExpr] = uop(et(op.toString.toLowerCase, op), a)
+
+  def uop[Arg <: Expr](op: MultiParser[Elem], a: MultiParser[Arg]): MultiParser[PrefixExpr] =
+    op ~ a ^^ {
+      case oper ~ arg => new PrefixExpr(arg, oper.getType)
+    }
 
   // tokens
 
-  def erlangToken(kind: String, t: ErlangToken) = token(kind, {
+  def et(t: ErlangToken): MultiParser[Elem] = et(t.toString.toLowerCase, t)
+
+  def et(kind: String, t: ErlangToken): MultiParser[Elem] = token(kind, {
     _.getType == t
   })
 
-  def erlangTextToken(kind: String, t: ErlangToken) = erlangToken(kind, t) ^^ {
+  def ett(kind: String, t: ErlangToken) = et(kind, t) ^^ {
     _.getText
   }
 
-  def dot = erlangToken("dot", DOT)
+  def dot = et("dot", DOT)
 
-  def lpar = erlangToken("lpar", PAR_LEFT)
+  def lpar = et("lpar", PAR_LEFT)
 
-  def rpar = erlangToken("rpar", PAR_RIGHT)
+  def rpar = et("rpar", PAR_RIGHT)
 
-  def atom = erlangTextToken("atom", ATOM)
+  def atomToken = ett("atom", ATOM)
 
-  def arrow = erlangToken("arrow", ARROW)
+  def variableToken = ett("var", VAR)
 
-  def comma = erlangToken("comma", COMMA)
+  def arrow = et("arrow", ARROW)
 
-  def semicolon = erlangToken("semicolon", SEMI)
+  def comma = et("comma", COMMA)
+
+  def semicolon = et("semicolon", SEMI)
 }
